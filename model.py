5dc873c6b3d10e0c1c33101cabea87009b1db7c0
comojin1994 <comojin1994@gmail.com>
2020-03-18 20:38:15 +0900

    Add initializer and regularizer

--- model.py
+++ model.py
@@ -1,32 +1,42 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPool2D, BatchNormalization, Dropout, Flatten, Dense
 
+def conv2d(filters, kernel_size, padding='same', activation=None):
+    return Conv2D(filters, kernel_size, padding=padding, activation=activation,
+                  kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.l2(0.001))
+
+def dense(filters, activation=None, name=None):
+    return Dense(filters, activation=activation,
+                 kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.l2(0.001), name=name)
+
 def inceptionLayer(x, filters):
-    h1 = Conv2D(filters[0], (1, 1), padding='same', activation='relu')(x)
+    h1 = conv2d(filters[0], (1, 1), padding='same', activation='relu')(x)
 
-    h2 = Conv2D(filters[1], (1, 1), padding='same')(x)
-    h2 = Conv2D(filters[2], (3, 3), padding='same', activation='relu')(h2)
+    h2 = conv2d(filters[1], (1, 1), padding='same')(x)
+    h2 = conv2d(filters[2], (3, 3), padding='same', activation='relu')(h2)
 
-    h3 = Conv2D(filters[3], (1, 1), padding='same', activation='relu')(x)
-    h3 = Conv2D(filters[4], (5, 5), padding='same', activation='relu')(h3)
+    h3 = conv2d(filters[3], (1, 1), padding='same', activation='relu')(x)
+    h3 = conv2d(filters[4], (5, 5), padding='same', activation='relu')(h3)
 
     h4 = MaxPool2D((3, 3), strides=(1, 1), padding='same')(x)
-    h4 = Conv2D(filters[5], (1, 1), padding='same')(h4)
+    h4 = conv2d(filters[5], (1, 1), padding='same')(h4)
 
     out = tf.keras.layers.Concatenate()([h1, h2, h3, h4])
     return out
 
-def googleNet(input_shape):
+def googleNet(input_shape, training=False):
     inputs = tf.keras.layers.Input(input_shape)
 
-    net = Conv2D(64, (3, 3), padding='same', activation='relu')(inputs)
+    net = conv2d(64, (3, 3), padding='same', activation='relu')(inputs)
     net = MaxPool2D()(net)
-    net = BatchNormalization()(net)
-    net = Dropout(0.3)(net)
+    if training:
+        net = BatchNormalization()(net)
+        net = Dropout(0.3)(net)
 
-    net = Conv2D(64, (1, 1), padding='same')(net)
-    net = Conv2D(192, (3, 3), padding='same')(net)
-    net = BatchNormalization()(net)
+    net = conv2d(64, (1, 1), padding='same')(net)
+    net = conv2d(192, (3, 3), padding='same')(net)
+    if training:
+        net = BatchNormalization()(net)
     net = tf.keras.layers.Activation('relu')(net)
     net = MaxPool2D()(net)
 
@@ -45,23 +55,26 @@ def googleNet(input_shape):
     net = inceptionLayer(net, (384, 192, 384, 48, 128, 128))
 
     out1 = MaxPool2D()(out1)
-    out1 = Conv2D(512, (1, 1), padding='same', activation='relu')(out1)
+    out1 = conv2d(512, (1, 1), padding='same', activation='relu')(out1)
     out1 = Flatten()(out1)
-    out1 = Dense(512, activation='relu')(out1)
-    out1 = Dropout(0.4)(out1)
-    out1 = Dense(10, activation='softmax', name='out1')(out1)
+    out1 = dense(512, activation='relu')(out1)
+    if training:
+        out1 = Dropout(0.4)(out1)
+    out1 = dense(10, activation='softmax', name='out1')(out1)
 
     out2 = MaxPool2D()(out2)
-    out2 = Conv2D(528, (1, 1), padding='same', activation='relu')(out2)
+    out2 = conv2d(528, (1, 1), padding='same', activation='relu')(out2)
     out2 = Flatten()(out2)
-    out2 = Dense(528, activation='relu')(out2)
-    out2 = Dropout(0.4)(out2)
-    out2 = Dense(10, activation='softmax', name='out2')(out2)
+    out2 = dense(528, activation='relu')(out2)
+    if training:
+        out2 = Dropout(0.4)(out2)
+    out2 = dense(10, activation='softmax', name='out2')(out2)
 
     out3 = MaxPool2D()(net)
-    out3 = Dropout(0.4)(out3)
+    if training:
+        out3 = Dropout(0.4)(out3)
     out3 = Flatten()(out3)
-    out3 = Dense(10, activation='softmax', name='out3')(out3)
+    out3 = dense(10, activation='softmax', name='out3')(out3)
 
     googlenet = tf.keras.Model(inputs=inputs, outputs=[out1, out2, out3])
     return googlenet
